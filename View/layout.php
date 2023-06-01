1b4174682793adb65bd02c020bdcb446ff7b10ac
HugoVanhoutte <116795821+HugoVanhoutte@users.noreply.github.com>
2023-06-01 16:44:20 +0100

    ajout bootstrap

--- View/layout.php
+++ View/layout.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://kit.fontawesome.com/9010a17673.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../../public/assets/style.css">
     <link rel="stylesheet" href="../public/assets/style.css">
     <!-- 2 links: second for homepage -->
@@ -45,5 +46,6 @@ if (isset($params['message'])) {
 
 <?= $pageContent ?>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
