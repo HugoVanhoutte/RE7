993624d3a8bc987ffd6ff54a7e8a36947a7a6ee2
PC fixe <hugo.vanhoutte.pro@gmail.com>
2023-06-14 00:17:06 +0200

    Modifications

--- View/layout.php
+++ View/layout.php
@@ -13,7 +13,7 @@
 <body>
 <nav class="navbar navbar-expand-md navbar-dark bg-primary mb-4">    <!--Navbar-->
     <div class="container">
-        <a href="/index.php" title="Accueil" class="navbar-brand"><span class="text-light">RE7</span></a> <!-- IDE error is wrong: link works on deployment -->
+        <a href="/index.php" title="Accueil" class="navbar-brand"><span class="text-light">RE7</span></a>
         <!-- Toggle button for mobile navigation -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav"><span
                     class="navbar-toggler-icon"></span></button>
@@ -24,23 +24,21 @@
                 <?php
                 if (!empty($_SESSION['user_id'])) {
                     ?>
-                        <li class="nav-item">
-                            <a href="/index.php/menu?action=new" title="Créer un menu" class="nav-link">Nouveau Menu</a>
-                        </li>
+                    <li class="nav-item">
+                        <a href="/index.php/menu?action=new" title="Créer un menu" class="nav-link">Nouveau Menu</a>
+                    </li>
 
-                        <li class="nav-item">
-                            <a href="/index.php/ingredient?action=new" title="Ajouter un ingrédient" class="nav-link">Nouvel Ingrédient</a>
-                        </li>
+                    <li class="nav-item">
+                        <a href="/index.php/ingredient?action=new" title="Ajouter un ingrédient" class="nav-link">Nouvel Ingrédient</a>
+                    </li>
 
                     <li class="nav-item">
                         <a href="/index.php/unit?action=new" title="Ajouter une unité" class="nav-link">Nouvelle Unité</a>
                     </li>
 
                     <li class="nav-item">
-                        <a href="/index.php/recipe?action=write" title="Créer une nouvelle recette" class="nav-link">Nouvelle
-                            Recette</a>
+                        <a href="/index.php/recipe?action=write" title="Créer une nouvelle recette" class="nav-link">Nouvelle Recette</a>
                     </li>
-
 
                     <li class="nav-item">
                         <a href="/index.php/user?action=profile&id=<?= $_SESSION['user_id'] ?>"
@@ -54,8 +52,7 @@
                 } else {
                     ?>
                     <li class="nav-item">
-                        <a href="/index.php/user?action=register" title="Créer un compte" class="nav-link">Créer un
-                            compte</a>
+                        <a href="/index.php/user?action=register" title="Créer un compte" class="nav-link">Créer un compte</a>
                     </li>
 
                     <li class="nav-item">
@@ -82,21 +79,24 @@ if (isset($params['message'])) {
 
 <?= $pageContent ?>
 
-<footer class="bg-dark text-light mt-5 container-fluid"> <!-- TODO -->
+<footer class="bg-dark text-light mt-5 container-fluid">
     <div class="row">
         <div class="col text-start">
             <ul class="list-unstyled">
                 <li><h3>Contact</h3></li>
-                <li><a href="mailto: hugo.vanhoutte.pro@gmail.com" title="Mail" class="text-muted display-6"><i class="fa-solid fa-envelope"></i></a></li>
-                <li><a href="#" title="Twitter" class="text-muted display-6"><i class="fa-brands fa-square-twitter"></i></a></li>
-                <li><a href="#" title="Facebook" class="text-muted display-6"><i class="fa-brands fa-square-facebook"></i></a></li>
+                <li><a href="mailto: hugo.vanhoutte.pro@gmail.com" title="Mail" class="text-muted display-6"><i
+                                class="fa-solid fa-envelope"></i></a></li>
+                <li><a href="#" title="Twitter" class="text-muted display-6"><i class="fa-brands fa-square-twitter"></i></a>
+                </li>
+                <li><a href="#" title="Facebook" class="text-muted display-6"><i
+                                class="fa-brands fa-square-facebook"></i></a></li>
             </ul>
         </div>
 
         <div class="col">
             <ul class="list-unstyled text-end">
-                    <li><h3>Mentions légales</h3></li>
-                    <li><a href="/index.php/user?action=rgpd" class="text-muted">RGPD</a></li>
+                <li><h3>Mentions légales</h3></li>
+                <li><a href="/index.php/user?action=rgpd" class="text-muted">RGPD</a></li>
             </ul>
         </div>
     </div>
